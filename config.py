820fc57ffa8a68ee40eedc59537264df24fe2106
Lomonoga <trintsa@mail.ru>
2025-09-23 04:45:03 +0500

    Config added

---
+++ config.py
@@ -0,0 +1,52 @@
+import logging
+import os
+from typing import ClassVar, Self, get_type_hints
+
+
+class Config:
+    _BOT_TOKEN_ENV: ClassVar[str] = 'Movie_And_Series_Watchlist_TELEGRAM_BOT_TOKEN'
+    _DATABASE_URL_ENV: ClassVar[str] = 'Movie_And_Series_Watchlist_DATABASE_URL'
+
+    BOT_TOKEN: ClassVar[str]
+    DATABASE_URL: ClassVar[str]
+
+    @classmethod
+    def _get_required_fields(cls) -> list[str]:
+        required = []
+        annotations = get_type_hints(cls, include_extras=True)
+        for name, annotation in annotations.items():
+            if not name.isupper():
+                continue
+            if hasattr(annotation, '__origin__') and annotation.__origin__ is ClassVar:
+                if not hasattr(cls, name):
+                    required.append(name)
+        return required
+
+    @classmethod
+    def _get_env(cls, name: str, required: bool = True) -> str | None:
+        value = os.getenv(name)
+        if not value and required:
+            logging.error(f'Environment variable "{name}" not found!')
+            logging.info(f'Set the environment variable: {name}.')
+            raise ValueError(f'Required environment variable "{name}" is not set.')
+        return value
+
+    @classmethod
+    def _load(cls):
+        cls.BOT_TOKEN = cls._get_env(cls._BOT_TOKEN_ENV)
+        cls.DATABASE_URL = cls._get_env(cls._DATABASE_URL_ENV)
+
+    @classmethod
+    def _validate(cls) -> None:
+        required_fields = cls._get_required_fields()
+        for field in required_fields:
+            if not getattr(cls, field, None):
+                raise ValueError(f'Field {field} is not set. Call Config.load() before using.')
+        logging.info('✅ The configuration has been successfully loaded and validated.')
+
+    @classmethod
+    def setup(cls) -> type[Self]:
+        cls._load()
+        cls._validate()
+
+        return cls
---
+++ main.py
@@ -0,0 +1,6 @@
+from telegram.ext import Application, Updater
+from config import Config
+
+config: Config = Config.setup()
+
+# application = Application.builder().token(BOT_TOKEN).build()
