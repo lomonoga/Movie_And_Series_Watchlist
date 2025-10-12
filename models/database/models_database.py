e9ff961b62be59e040be93f25489bc3d1a0089c1
Lomonoga <trintsa@mail.ru>
2025-10-12 22:28:07 +0500

    added relationship on models

--- models/database/models_database.py
+++ models/database/models_database.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, DateTime, Boolean, BigInteger, UUID, ARRAY, Enum, Float
+from sqlalchemy import Column, String, DateTime, Boolean, BigInteger, UUID, ARRAY, Enum, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from database.postgresql_database import Base
@@ -6,9 +7,9 @@ from models.logic.GenreOfFilmEnum import GenreOfFilmEnum
 
 
 class User(Base):
-    __tablename__ = 'Users'
+    __tablename__ = 'users'
 
-    user_id = Column(BigInteger, primary_key=True, index=True, nullable=False)
+    id = Column(BigInteger, primary_key=True, index=True, nullable=False)
     is_bot = Column(Boolean, index=True, nullable=False)
     first_name = Column(String(255), nullable=False)
 
@@ -17,22 +18,47 @@ class User(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-    def __repr__(self):
-        return f"<User(id={self.user_id}, is_bot={self.is_bot}, name={self.first_name})>"
+    # Relationships
+    playlists = relationship('Playlist', back_populates='user', order_by='Playlist.updated_at')
+    movies = relationship('Movie', back_populates='user', order_by='Movie.imdb_rating')
 
 
 class Movie(Base):
-    __tablename__ = 'Movies'
+    __tablename__ = 'movies'
 
-    internal_movie_id = Column(UUID, primary_key=True, index=True, nullable=False)
+    internal_id = Column(UUID, primary_key=True, index=True, nullable=False)
     manual_title = Column(String(255), nullable=False)
     manual_rating = Column(Float, nullable=False)
 
     omdb_genres = Column(ARRAY(Enum(GenreOfFilmEnum)), nullable=False)
     imdb_rating = Column(Float, nullable=False)
 
+    user_id = Column(BigInteger, ForeignKey('users.id'), nullable=False)
+    playlist_id = Column(UUID, ForeignKey('playlists.id'), nullable=False)
+
     is_viewed = Column(Boolean, nullable=False, default=False)
 
     is_deleted = Column(Boolean, index=True, nullable=False, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # Relationships
+    user = relationship('User', back_populates='movies')
+    playlist = relationship('Playlist', back_populates='movies')
+
+
+class Playlist(Base):
+    __tablename__ = 'playlists'
+
+    id = Column(UUID, primary_key=True, index=True, nullable=False)
+    name = Column(String(255), nullable=False)
+
+    user_id = Column(BigInteger, ForeignKey('users.id'), nullable=False)
+
+    is_deleted = Column(Boolean, index=True, nullable=False, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # Relationships
+    user = relationship('User', back_populates='playlists')
+    movies = relationship('Movie', back_populates='playlist', order_by='Movie.imdb_rating')
